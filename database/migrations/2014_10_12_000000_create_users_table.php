76a6e2b6543f1573a997fccb22ede94d99403430
damzylee <rufaidamilolaibraheem1995@gmail.com>
2019-11-08 21:10:25 +0100

    Initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('number')->nullable();
+            $table->string('type')->default('user');
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+            $table->string('town')->nullable();
+            $table->boolean('is_admin')->nullable();
+            $table->string('image')->nullable();
+            $table->string('BIOS')->nullable();
+            $table->date('DOB')->nullable();
+            $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2019_11_08_200559_create_companies_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->unsignedInteger('subscription_id')->nullable();
+            $table->string('number')->nullable();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->string('type');
+            $table->string('country');
+            $table->string('state');
+            $table->string('address');
+            $table->string('image')->nullable();
+            $table->string('profile')->nullable();
+            $table->boolean('confirm')->nullable();
+            $table->date('YOE')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
